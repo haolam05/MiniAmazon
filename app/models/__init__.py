3fb113144932e618f3dc241adbb5068e44beb906
Hao Lam <tuonghao2001@gmail.com>
2024-02-27 15:23:53 -0800

    create bookmarks table

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,4 @@ from .db import environment, SCHEMA
 from .customer import Customer
 from .product import Product
 from .review import Review
+from .bookmark import Bookmark
---
+++ app/models/bookmark.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+class Bookmark(db.Model):
+    __tablename__ = "bookmarks"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    note = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+
+    product_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("products.id")), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("customers.id")), nullable=False)
+
+
+    """ one-to-many """
+    product = db.relationship("Product", back_populates="bookmarks")
+    customer = db.relationship("Customer", back_populates="bookmarks")
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "product_id": self.product_id,
+            "customer_id": self.customer_id,
+            "note": self.note,
+            "created_at": str(self.created_at)
+        }
