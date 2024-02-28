343a39ac55fa555576f1176d35aed9cfbb333191
Hao Lam <tuonghao2001@gmail.com>
2024-02-28 00:29:51 -0800

    seeded orders table

--- app/seeds/orders.py
+++ app/seeds/orders.py
@@ -1,10 +1,22 @@
-from app.models import db, environment, SCHEMA, Order
+from app.models import db, environment, SCHEMA, Customer, Order
 from sqlalchemy.sql import text
 
 
 def seed_orders():
+    username_to_ids = Customer.username_to_ids()
 
     orders = [
+        {
+            "customer_id": username_to_ids["haolam"],
+        },
+        {
+            "customer_id": username_to_ids["haolam"],
+            "is_checkout": True
+        },
+        {
+            "customer_id": username_to_ids["haolam"],
+            "is_checkout": True
+        }
     ]
 
     [db.session.add(Order(**order)) for order in orders]
