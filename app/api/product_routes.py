85df571d73aff6975b8d0263be356bb507df7858
Hao Lam <tuonghao2001@gmail.com>
2024-02-28 12:37:30 -0800

    update product route

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -60,40 +60,43 @@ def create_product():
     return form.errors, 400
 
 
-# @product_routes.route('/<int:id>', methods=['PUT'])
-# @login_required
-# def update_product(id):
-#     """Update an exisiting product by id"""
-#     form = ProductForm()
-#     form["csrf_token"].data = request.cookies["csrf_token"]
+@product_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def update_product(id):
+    """Update an exisiting product by id"""
+    form = ProductForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
 
-#     if form.validate_on_submit():
-#         product = Product.query.get(id)
-#         image = form.data["product_image"]
-#         url = None
+    if form.validate_on_submit():
+        product = Product.query.get(id)
+        image = form.data["product_image"]
+        url = None
 
-#         if not product:
-#             return {"message": "Product couldn't be found"}, 404
+        if not product:
+            return {"message": "Product couldn't be found"}, 404
 
-#         if image:
-#             image.filename = get_unique_filename(image.filename)
-#             upload = upload_file_to_s3(image)
-#             if "url" not in upload:
-#                 return {"product_image": "Image upload fail. Please try again later."}, 500
-#             url = upload["url"]
+        if product.seller_id != current_user.id:
+            return redirect("/api/auth/forbidden")
 
-#         product.name = form.data["name"],
-#         product.category = form.data["category"],
-#         product.description = form.data["description"],
-#         product.price = form.data["price"],
-#         product.remaining = form.data["remaining"],
-#         product.product_image = url
+        if image:
+            image.filename = get_unique_filename(image.filename)
+            upload = upload_file_to_s3(image)
+            if "url" not in upload:
+                return {"product_image": "Image upload fail. Please try again later."}, 500
+            url = upload["url"]
 
-#         db.session.commit()
+        product.name = form.data["name"]
+        product.category = form.data["category"]
+        product.description = form.data["description"]
+        product.price = form.data["price"]
+        product.remaining = form.data["remaining"]
+        product.product_image = url
 
-#         return product.to_dict(), 200
+        db.session.commit()
 
-#     return form.errors, 400
+        return product.to_dict(), 200
+
+    return form.errors, 400
 
 
 @product_routes.route('/<int:id>', methods=['DELETE'])
