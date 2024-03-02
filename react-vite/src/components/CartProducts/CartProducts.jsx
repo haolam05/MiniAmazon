cd0142f4192283fd16c81adfe4ee63923755bd0e
Hao Lam <tuonghao2001@gmail.com>
2024-03-01 16:50:24 -0800

    styling scrollbar

--- react-vite/src/components/CartProducts/CartProducts.jsx
+++ react-vite/src/components/CartProducts/CartProducts.jsx
@@ -1,12 +1,28 @@
+import { getFormattedPrice } from "../../utils/product";
 import CartProduct from "../CartProduct";
 import "./CartProducts.css";
 
 function CartProducts({ products, itemsInCart }) {
+  const getSubTotal = items => {
+    return items.reduce((sum, item) => sum + (+products[item.product_id].price * item.quantity), 0);
+  }
+
   return (
     <div id="cart-products">
-      {itemsInCart.map(item => {
+      <div className="summary">
+        <div className="subtotal">
+          <span className="text">Subtotal</span>
+          <span className="dollar-sign">$</span>
+          <span className="price">{getFormattedPrice(`${getSubTotal(itemsInCart)}`)[0]}</span>
+          <span className="decimal">{getFormattedPrice(`${getSubTotal(itemsInCart)}`)[1]}</span>
+        </div>
+        <div className="checkout">
+          <p>Proceed to checkout</p>
+        </div>
+      </div>
+      {[...itemsInCart, ...itemsInCart, ...itemsInCart].map(item => {
         const product = products[item.product_id];
-        return <CartProduct product={product} />
+        return <CartProduct key={`cart-product-${item.id}`} product={product} />
       })}
     </div>
   );
