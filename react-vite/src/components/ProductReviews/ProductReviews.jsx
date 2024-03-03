77fe7e7eb6ddb43d329caa51830d9a4fa3d98a95
Hao Lam <tuonghao2001@gmail.com>
2024-03-03 02:09:54 -0800

    write a review setup

--- react-vite/src/components/ProductReviews/ProductReviews.jsx
+++ react-vite/src/components/ProductReviews/ProductReviews.jsx
@@ -4,8 +4,17 @@ import "./ProductReviews.css";
 function ProductReviews({ product, user }) {
   if (!product) return;
 
+  const userHasWrittenReview = () => {
+    return product.reviews.map(review => review.customer_id).includes(user.id);
+  }
+
   return (
     <div id="product-reviews">
+      {!userHasWrittenReview() && (
+        <div className="new-review-icon" title="Write a review">
+          <i className="fa-solid fa-pencil"></i>
+        </div>
+      )}
       {product.reviews.map(review => (
         <ProductReview key={review.id} review={review} user={user} />
       ))}
