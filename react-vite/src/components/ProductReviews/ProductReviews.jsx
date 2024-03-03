6b82893149047084b7c44bf6bf1291ee8f75affa
Hao Lam <tuonghao2001@gmail.com>
2024-03-03 10:27:20 -0800

    always show current user review at the front

--- react-vite/src/components/ProductReviews/ProductReviews.jsx
+++ react-vite/src/components/ProductReviews/ProductReviews.jsx
@@ -4,18 +4,17 @@ import "./ProductReviews.css";
 function ProductReviews({ product, user }) {
   if (!product) return;
 
-  const userHasWrittenReview = () => {
-    return product.reviews.map(review => review.customer_id).includes(user.id);
-  }
+  const otherReviews = product.reviews.filter(review => review.customer_id !== user.id);
+  const myReview = product.reviews.filter(review => review.customer_id === user.id);
 
   return (
     <div id="product-reviews">
-      {!userHasWrittenReview() && (
+      {!myReview && (
         <div className="new-review-icon" title="Write a review">
           <i className="fa-solid fa-pencil"></i>
         </div>
       )}
-      {product.reviews.map(review => (
+      {[...myReview, ...otherReviews].map(review => (
         <ProductReview key={review.id} review={review} user={user} />
       ))}
     </div>
