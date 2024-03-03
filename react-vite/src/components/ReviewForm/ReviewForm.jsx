30411f3d182d098b0b4fac2baeab265572b367c2
Hao Lam <tuonghao2001@gmail.com>
2024-03-03 15:42:50 -0800

    changing scroll to auto to hide scrollbar when not scrolling

--- react-vite/src/components/ReviewForm/ReviewForm.jsx
+++ react-vite/src/components/ReviewForm/ReviewForm.jsx
@@ -1,20 +1,63 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { handleNoteOnChange } from "../../utils/form";
+import { useModal } from "../../context/Modal";
 import { disabledSubmitButton, enabledSubmitButton } from "../../utils/dom";
+import NotificationModal from "../NotificationModal";
 import * as productActions from "../../redux/product";
 
 function ReviewForm({ productId }) {
   const dispatch = useDispatch();
+  const { setModalContent } = useModal();
+  const [note, setNote] = useState("");
+  const [errors, setErrors] = useState({});
+  const [submitting, setSubmitting] = useState(false);
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault();
+
     disabledSubmitButton();
+    setSubmitting(true);
 
+    const data = await dispatch(bookmarkActions.createBookmarkThunk(productId, note));
 
+    if (data?.errors) {
+      setSubmitting(false);
+      enabledSubmitButton();
+      return setErrors(data.errors);
+    }
+
+    setSubmitting(false);
     enabledSubmitButton();
+    setModalContent(
+      <NotificationModal
+        message="Successfully created bookmark"
+        status="alert-success"
+      />
+    );
   }
 
   return (
-    <></>
+    <>
+      <h2 className="subheading">Write a review</h2>
+      <form id="bookmark-form" onSubmit={handleSubmit}>
+        <textarea
+          spellCheck={false}
+          placeholder="Add a note to your bookmark"
+          value={note}
+          onChange={e => handleNoteOnChange(e, "note", setNote, setErrors)}
+        />
+        {errors.note && <p className="modal-errors">{errors.note}</p>}
+        {submitting && <div className="loader"></div>}
+        <button
+          type="submit"
+          className={note.length ? "" : "disabled"}
+          disabled={!note.length}
+        >
+          Submit
+        </button>
+      </form>
+    </>
   );
 }
 
