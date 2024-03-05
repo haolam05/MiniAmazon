a444dcf61d57d0eaa4cc5e885e90f6374f8d82d3
Hao Lam <tuonghao2001@gmail.com>
2024-03-04 21:15:33 -0800

    close notification modal automatically after 4s

--- react-vite/src/components/NotificationModal/NotificationModal.jsx
+++ react-vite/src/components/NotificationModal/NotificationModal.jsx
@@ -1,10 +1,24 @@
+import { useEffect } from "react";
+import { useModal } from "../../context/Modal";
 import "./NotificationModal.css";
 
 function NotificationModal({ message, status }) {
+  const { closeModal } = useModal();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      closeModal();
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div id="notification">
       <h2 className="subheading">Notification</h2>
       <p className={status}>{message}</p>
+      <div className="loading">
+        <div className="loader"></div>
+      </div>
     </div>
   );
 }
