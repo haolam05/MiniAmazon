074d39b68ea11634fcac44d5cd1dbeee4400b8a6
Hao Lam <tuonghao2001@gmail.com>
2024-03-05 14:53:23 -0800

    magnifying image done

--- react-vite/src/components/ProductDetails/ProductDetails.jsx
+++ react-vite/src/components/ProductDetails/ProductDetails.jsx
@@ -17,8 +17,16 @@ function ProductDetails({ user, product, createAndShowBookmarks, showCart, inCar
   const showMagnifyImage = e => {
     const len = document.querySelector("#product-details .image-len");
     const image = document.querySelector("#product-details .product-image img");
+    const imageMagnifyContainer = document.querySelector("#product-details .product-image-magnify");
     const lenInfo = len.getBoundingClientRect();
     const imageInfo = image.getBoundingClientRect();
+    const paddingX = 5;  // 5px horizontal padding of parent container
+    const paddingY = 10; // 10px vertical padding of parent container
+
+    if (!len || !image || !imageMagnifyContainer) return;
+
+    len.classList.remove("hidden");
+    imageMagnifyContainer.classList.remove("hidden")
 
     if (image && len) {
       // cursorX = the width between left of screen (viewport) to current cursor position
@@ -38,8 +46,8 @@ function ProductDetails({ user, product, createAndShowBookmarks, showCart, inCar
       // offsetX = the width between the start of the image from the left side to the cursor position (left)
       // offsetY = the height between the start of the image from the top side to the cursor position (top)
       let [offsetX, offsetY] = [
-        cursorX - imageX + 5, // 5px horizontal padding of parent container
-        cursorY - imageY + 10 // 10px vertical padding of parent container
+        cursorX - imageX + paddingX,
+        cursorY - imageY + paddingY
       ];
 
       // imageWidth = width of the image (including border and padding)
@@ -49,13 +57,37 @@ function ProductDetails({ user, product, createAndShowBookmarks, showCart, inCar
         imageInfo.height
       ];
 
+      // prevent len to get out-of-bound
       if (offsetX < 0) offsetX = 0;
       if (offsetY < 0) offsetY = 0;
-      if (offsetX > imageWidth - lenInfo.width) offsetX = imageWidth - lenInfo.width + 5;
-      if (offsetY > imageHeight - lenInfo.height) offsetY = imageHeight - lenInfo.height + 10;
+      if (offsetX > imageWidth - lenInfo.width) offsetX = imageWidth - lenInfo.width + paddingX;
+      if (offsetY > imageHeight - lenInfo.height) offsetY = imageHeight - lenInfo.height;
 
+      // set len position
       len.style.left = offsetX + "px";
       len.style.top = offsetY + "px";
+
+      // Ratio between len & the container of the magnified image
+      const ratioX = imageMagnifyContainer.offsetWidth / len.offsetWidth;
+      const ratioY = imageMagnifyContainer.offsetHeight / len.offsetHeight;
+
+      // backgroundImage = set maginified image
+      // backgroundSize = zoom magnified image based on ratio calculated above
+      // backgroundPosition = position the background image(top left) to the correct position based on the len
+      //                    = the maginify container doesn't have padding ---> subtract
+      //                    = minus: based on top-left
+      imageMagnifyContainer.style.backgroundImage = `url('${image.src}')`;
+      imageMagnifyContainer.style.backgroundSize = `${imageWidth * ratioX}px ${imageHeight * ratioY}px`;
+      imageMagnifyContainer.style.backgroundPosition = `-${(offsetX - paddingX) * ratioX}px -${(offsetY - paddingY) * ratioY}px`;
+    }
+  }
+
+  const hideMagnifyImage = () => {
+    const len = document.querySelector("#product-details .image-len");
+    const imageMagnifyContainer = document.querySelector("#product-details .product-image-magnify");
+    if (len && imageMagnifyContainer) {
+      len.classList.add("hidden");
+      imageMagnifyContainer.classList.add("hidden")
     }
   }
 
@@ -63,14 +95,12 @@ function ProductDetails({ user, product, createAndShowBookmarks, showCart, inCar
     <>
       <h2 className="product-title subheading">{product.name}</h2>
       <div id="product-details">
-        <div className="product-image-magnify">
-          <img src={product.product_image} alt="product-image-magnify" />
-        </div>
+        <div className="product-image-magnify hidden"></div>
         <div className="product-wrapper">
           <div className="product cursor-normal" id={product.id}>
             <div className="product-image">
-              <div className="image-len"></div>
-              <img src={product.product_image} alt="product-image" onMouseMove={showMagnifyImage} />
+              <div className="image-len hidden" onMouseMove={showMagnifyImage}></div>
+              <img src={product.product_image} alt="product-image" onMouseMove={showMagnifyImage} onMouseLeave={hideMagnifyImage} />
               <ProductReviewInfo
                 user={user}
                 product={product}
