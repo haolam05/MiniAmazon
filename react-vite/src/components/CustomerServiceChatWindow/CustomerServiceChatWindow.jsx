6eb4309b91a29d564f8496e7a6c1d587a3cf2e87
Hao Lam <tuonghao2001@gmail.com>
2024-03-06 21:40:17 -0800

    styling bugfix

--- react-vite/src/components/CustomerServiceChatWindow/CustomerServiceChatWindow.jsx
+++ react-vite/src/components/CustomerServiceChatWindow/CustomerServiceChatWindow.jsx
@@ -1,17 +1,15 @@
 import { closeChat } from "../../utils/chat";
-import ChatMessages from "../ChatMessages/ChatMessages";
+import ChatMessages from "../ChatMessages";
 import "./CustomerServiceChatWindow.css";
 
 function CustomerServiceChatWindow() {
   return (
     <div id="customer-service-chat-window" className="hidden">
       <div className="chat-title">
-        <h2 className="subheading">
-          <div className="subheading">Customer Service</div>
-        </h2>
-        <div className="close-chat-btn" title="Close chat" onClick={closeChat}>
-          <i className="fa-solid fa-left-long"></i>
-        </div>
+        <div className="subheading">Customer Service</div>
+      </div>
+      <div className="close-chat-btn" title="Close chat" onClick={closeChat}>
+        <i className="fa-solid fa-left-long"></i>
       </div>
       <div className="chat-body">
         <ChatMessages messages={[]} />
