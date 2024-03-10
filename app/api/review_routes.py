48587082e0c8623eddd1f1aec92e188813127808
Hao Lam <tuonghao2001@gmail.com>
2024-03-09 17:00:31 -0800

    live update review

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, redirect
 from flask_login import login_required, current_user
-from app.models import db, Review
+from app.models import db, Review, Product
 from app.forms import ReviewForm
+from ..socket import socketio
 
 
 review_routes = Blueprint('reviews', __name__)
@@ -38,6 +39,7 @@ def update_review(id):
 
         db.session.commit()
 
+        socketio.emit("product_review_update", {"product_owner_id": current_user.id, "product": review.product.to_dict()})
         return {**review.to_dict(), "customer": review.customer.to_dict()}, 200
 
     return form.errors, 400
@@ -48,6 +50,7 @@ def update_review(id):
 def delete_review(id):
     """Delete a review by id"""
     review = Review.query.get(id)
+    product = Product.query.get(review.product_id)
 
     if not review:
         return {"message": "review couldn't be found"}, 404
@@ -58,4 +61,5 @@ def delete_review(id):
     db.session.delete(review)
     db.session.commit()
 
+    # socketio.emit("product_review_delete", {"product_owner_id": current_user.id, "product": product.to_dict()})
     return {"message": "Successfully deleted review"}, 200
--- react-vite/src/components/HomePage/HomePage.jsx
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -117,7 +117,7 @@ function HomePage() {
         }
       }
 
-      const handleProductReviewCreate = data => {
+      const handleProductReviewCUD = data => {
         if (data.review_owner_id !== user?.user?.id) {
           dispatch(productActions.updateProductsWhenProductIsUpdatedThunk(data.product));
         }
@@ -127,7 +127,9 @@ function HomePage() {
       socket.on("product_delete", handleProductDelete);
       socket.on("product_update", handleProductUpdate);
       socket.on("product_create", handleProductCreate);
-      socket.on("product_review_create", handleProductReviewCreate);
+      socket.on("product_review_create", handleProductReviewCUD);
+      socket.on("product_review_update", handleProductReviewCUD);
+      // socket.on("product_review_delete", handleProductReviewCUD);
 
       await dispatch(sessionActions.restoreSession());
       await dispatch(productActions.loadProductsThunk());
