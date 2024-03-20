8489d54a990ca6362e86180b7664713f8cabf7e8
Hao Lam <tuonghao2001@gmail.com>
2024-03-20 11:51:28 -0700

    refactor

--- react-vite/src/components/Products/Products.jsx
+++ react-vite/src/components/Products/Products.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Product from "../Product/Product";
+import Product from "../Product";
 import "./Products.css";
 
 function Products({ products, user, inCartProductIds, bookmarkProductIds, productNameInput, productCategoryInput }) {
